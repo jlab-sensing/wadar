f69d1171f8c1fead5d93d770b7051f5ba490dba2
Colleen Josephson <cajoseph@stanford.edu>
2019-05-28 16:08:26 -0700

    added cayenne frameLogger files to c_code dir

--- matlab/SalsaLab/soil_moisture.m
+++ matlab/SalsaLab/soil_moisture.m
@@ -1,12 +1,11 @@
 % Script to calculate VWC from ToF and teros-12 sensor measurements
 c=299792458;
 resolution =  0.004; %0.003790984152165;
-airBins = 199; % number of bins from the radar to the un-burried tag
-airSoilBins = 229; % number of bins from the radar to the burried tag
-RAW = 2483;  %RAW teros12 measurement
-d=0.05; %meters
+airBins = 215; % number of bins from the radar to the un-burried tag
+airSoilBins = (240+240+239)/3.0; % number of bins from the radar to the burried tag
+RAW = 2345;  %RAW teros12 measurement
+d=0.055; %meters
 t=((airSoilBins-airBins+d/resolution)*resolution)/c; %seconds
-airD = 0.45;
 
 radar_perm = ((c*t)/d)^2
 teros_perm = ((2.887e-9)*RAW^3-(2.080e-5)*RAW^2+(5.276e-2)*RAW-43.39)^2
