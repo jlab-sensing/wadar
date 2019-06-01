fe98ed864565124df9020e0eb6dddd329359627e
Bradley Barnhart <bbarnhar@stanford.edu>
2019-05-31 18:25:11 -0700

    modified salsaData to also load from Ancho captures

--- matlab/SalsaLab/salsaData.m
+++ matlab/SalsaLab/salsaData.m
@@ -77,13 +77,24 @@ if  isDataCaptured == 0 % load
         dacMax = fread(fid,1,'int');
         dacStep = fread(fid,1,'int');
 
-        % The measured sampling rate is next
-        samplesPerSecond = fread(fid,1,'float');
-        % The NVA6201 specific settings are next
-        pgen = fread(fid,1,'int');
-        % The frame offset, reference delay
-        offsetDistance = fread(fid,1,'float');
-        sampleDelayToReference = fread(fid,1,'float');
+        % The radar type is next 
+        isAncho = fread(fid,1,'bool'); 
+        if isAncho
+            % The measured sampling rate is next
+            samplesPerSecond = fread(fid,1,'float');
+            % The NVA6201 specific settings are next
+            pgen = fread(fid,1,'int');
+            % The frame offset, reference delay
+            offsetDistance = fread(fid,1,'float');
+            sampleDelayToReference = fread(fid,1,'float');
+        else
+            % The measured sampling rate is next
+            samplesPerSecond = fread(fid,1,'double');
+            % The NVA6201 specific settings are next
+            pgen = fread(fid,1,'int');
+            samplingRate = fread(fid,1,'int');
+            clkDivider = fread(fid,1,'int');
+        end
         % Next is the #samplers in a frame
         numberOfSamplers = fread(fid,1,'int');
         % Determine #frames in capture
@@ -106,131 +117,142 @@ if  isDataCaptured == 0 % load
         end
         frameCount = size(frameTot,2);
     end
+    
+    if isAncho
+        radarSpecifier = "X2"; 
+    else
+        radarSpecifier = "X1-IPG0"; 
+    end
+    
+    [fc, bw, bwr, vp, n, bw_hz, pwr_dBm, fs_hz] = NoveldaChipParams(radarSpecifier, pgen,'4mm');
+    timeTot = (numFrames - 1) / frameRate; 
 
 else %capture
-    % INITIALIZE RADAR SETTINGS --------------------------------------------------------------------
-    fprintf('Initializing radar settings'); 
-
-    % Create the radar object
-    radar = radarWrapper('192.168.7.2'); %USB Cable
-
-    % Get a list of the connected modules
-    modules = radar.ConnectedModules;
-
-    % Open a connection to the radar module
-    radar.Open(modules{1});
-
-    % Set some register values 
-    radar.TryUpdateChip('Iterations','16');
-    radar.TryUpdateChip('DACMin','4300');
-    radar.TryUpdateChip('DACMax','4500');
-    radar.TryUpdateChip('DACStep','8');
-    radar.TryUpdateChip('PulsesPerStep','8');
-    radar.TryUpdateChip('FrameStitch','1');
-    % Changing PRF: PRFDivide divides the default
-    % 100Mhz PRF. So setting  to 2 yields PRF = 50Mhz
-    % radar.TryUpdateChip('PRFDivide','6');
-
-    % Set some specific radarlib3 settings
-    if (strcmp(radarType,'ancho'))
-        pgen = 0; %Ancho center frequency 
-        radar.TryUpdateChip('PGSelect', pgen);
-        radar.SetVoltage(1.2);
-
-        sampleDelayToReference = 3.687e-9; 
-        radarSpecifier = 'X2'; 
-    elseif (strcmp(radarType, 'cayenne')) 
-        pgen = 0; %Cayenne center frequency???
-        radar.TryUpdateChip('PulseGen', '4.3GHz'); %1.5GHz, 4.3GHz 
-        radar.TryUpdateChip('PulseGenFineTune', pgen); 
-        radar.TryUpdateChip('SamplingRate', 0); %0 for 26 ps
-
-        sampleDelayToReference = 1.5e-9; 
-        radarSpecifier = 'X1-IPGO'; 
-    end
-
-    % Calibrate the radar module
-    tic
-    result = radar.ExecuteAction('MeasureAll');
-    toc
-
-    % Now set the OffsetDistanceFromReference and/or SampleDelayToReference 
-    % NOTE -- these requires a calibration first!
-
-    % Set the SampleDelayToReference (a value effected by antenna/cable choice)
-    radar.TryUpdateChip('SampleDelayToReference', sampleDelayToReference);
-
-    % Get some radar values
-    iterations = radar.Item('Iterations');
-    offsetdistance = radar.Item('OffsetDistanceFromReference');
-    samplers = radar.Item('SamplersPerFrame');
-    [fc, bw, bwr, vp, n, bw_hz, pwr_dBm, fs_hz] = NoveldaChipParams(radarSpecifier, pgen,'4mm');
-
-    resolution = radar.SamplerResolution;
-    range = linspace(0, samplers * resolution, samplers);
-
-    % COLLECT FRAMES FOR DESIRED MAXTIME -----------------------------------------------------------
-    fpsTarget = 200; 
-    Ts = 1 / fpsTarget; 
-    maxTime = 10.00;  
-    maxFrames = fpsTarget * maxTime + 1; 
-
-    frameCount = 0; 
-    timeStart = tic;
-
-    fprintf('Capturing frames for %0.2f seconds', maxTime);
-    while (toc(timeStart) < maxTime) 
-        % Collect radar frame 
-        newFrame1 = radar.GetFrameNormalizedDouble;
-        captureTime = toc(timeStart); 
-        newFrame1 = newFrame1'; %column vector 
-
-        if (frameCount == 0)
-            frameTot = zeros(size(newFrame1,1), maxFrames);
-            timeTot = zeros(1, maxFrames); 
-        end
-
-        % Store frame and time data
-        frameCount = frameCount + 1;
-        frameTot(:,frameCount) = newFrame1; 
-        timeTot(frameCount) = captureTime;
-
-        % Control fps
-        while (toc(timeStart) < frameCount * Ts)
-            %wait 
-        end
-    end
-
-    % Truncate to last captured frame
-    frameTot = frameTot(:,1:frameCount); 
-    timeTot = timeTot(1:frameCount); 
-    timeTot = timeTot - timeTot(1); % set initial capture to t=0
-
-    % Calculate fps 
-    timeElapsed = timeTot(end);
-    fpsEst = (size(frameTot,2) - 1) / timeElapsed;
-    fprintf('Estimated FPS: %f',fpsEst);
-    
-    % SAVE THE DATA --------------------------------------------------------------------------------
-    if ~strcmp(savePath, 'default')
-        fprintf('Saving output to %s...\n',savePath)
-        save(sprintf(strcat(savePath,'expData%s.mat'),datestr(now,30)), ...
-            'maxTime','frameCount','frameTot','timeTot','fpsEst','radarType','pgen')
-    end
+      % TODO: Call C code 
+%     % INITIALIZE RADAR SETTINGS --------------------------------------------------------------------
+%     fprintf('Initializing radar settings'); 
+% 
+%     % Create the radar object
+%     radar = radarWrapper('192.168.7.2'); %USB Cable
+% 
+%     % Get a list of the connected modules
+%     modules = radar.ConnectedModules;
+% 
+%     % Open a connection to the radar module
+%     radar.Open(modules{1});
+% 
+%     % Set some register values 
+%     radar.TryUpdateChip('Iterations','16');
+%     radar.TryUpdateChip('DACMin','4300');
+%     radar.TryUpdateChip('DACMax','4500');
+%     radar.TryUpdateChip('DACStep','8');
+%     radar.TryUpdateChip('PulsesPerStep','8');
+%     radar.TryUpdateChip('FrameStitch','1');
+%     % Changing PRF: PRFDivide divides the default
+%     % 100Mhz PRF. So setting  to 2 yields PRF = 50Mhz
+%     % radar.TryUpdateChip('PRFDivide','6');
+% 
+%     % Set some specific radarlib3 settings
+%     if (strcmp(radarType,'ancho'))
+%         pgen = 0; %Ancho center frequency 
+%         radar.TryUpdateChip('PGSelect', pgen);
+%         radar.SetVoltage(1.2);
+% 
+%         sampleDelayToReference = 3.687e-9; 
+%         radarSpecifier = 'X2'; 
+%     elseif (strcmp(radarType, 'cayenne')) 
+%         pgen = 0; %Cayenne center frequency???
+%         radar.TryUpdateChip('PulseGen', '4.3GHz'); %1.5GHz, 4.3GHz 
+%         radar.TryUpdateChip('PulseGenFineTune', pgen); 
+%         radar.TryUpdateChip('SamplingRate', 0); %0 for 26 ps
+% 
+%         sampleDelayToReference = 1.5e-9; 
+%         radarSpecifier = 'X1-IPGO'; 
+%     end
+% 
+%     % Calibrate the radar module
+%     tic
+%     result = radar.ExecuteAction('MeasureAll');
+%     toc
+% 
+%     % Now set the OffsetDistanceFromReference and/or SampleDelayToReference 
+%     % NOTE -- these requires a calibration first!
+% 
+%     % Set the SampleDelayToReference (a value effected by antenna/cable choice)
+%     radar.TryUpdateChip('SampleDelayToReference', sampleDelayToReference);
+% 
+%     % Get some radar values
+%     iterations = radar.Item('Iterations');
+%     offsetdistance = radar.Item('OffsetDistanceFromReference');
+%     samplers = radar.Item('SamplersPerFrame');
+%     [fc, bw, bwr, vp, n, bw_hz, pwr_dBm, fs_hz] = NoveldaChipParams(radarSpecifier, pgen,'4mm');
+% 
+%     resolution = radar.SamplerResolution;
+%     range = linspace(0, samplers * resolution, samplers);
+% 
+%     % COLLECT FRAMES FOR DESIRED MAXTIME -----------------------------------------------------------
+%     fpsTarget = 200; 
+%     Ts = 1 / fpsTarget; 
+%     maxTime = 10.00;  
+%     maxFrames = fpsTarget * maxTime + 1; 
+% 
+%     frameCount = 0; 
+%     timeStart = tic;
+% 
+%     fprintf('Capturing frames for %0.2f seconds', maxTime);
+%     while (toc(timeStart) < maxTime) 
+%         % Collect radar frame 
+%         newFrame1 = radar.GetFrameNormalizedDouble;
+%         captureTime = toc(timeStart); 
+%         newFrame1 = newFrame1'; %column vector 
+% 
+%         if (frameCount == 0)
+%             frameTot = zeros(size(newFrame1,1), maxFrames);
+%             timeTot = zeros(1, maxFrames); 
+%         end
+% 
+%         % Store frame and time data
+%         frameCount = frameCount + 1;
+%         frameTot(:,frameCount) = newFrame1; 
+%         timeTot(frameCount) = captureTime;
+% 
+%         % Control fps
+%         while (toc(timeStart) < frameCount * Ts)
+%             %wait 
+%         end
+%     end
+% 
+%     % Truncate to last captured frame
+%     frameTot = frameTot(:,1:frameCount); 
+%     timeTot = timeTot(1:frameCount); 
+%     timeTot = timeTot - timeTot(1); % set initial capture to t=0
+% 
+%     % Calculate fps 
+%     timeElapsed = timeTot(end);
+%     fpsEst = (size(frameTot,2) - 1) / timeElapsed;
+%     fprintf('Estimated FPS: %f',fpsEst);
+%     
+%     % SAVE THE DATA --------------------------------------------------------------------------------
+%     % TODO: save data 
+%     if ~strcmp(savePath, 'default')
+%         fprintf('Saving output to %s...\n',savePath)
+%         save(sprintf(strcat(savePath,'expData%s.mat'),datestr(now,30)), ...
+%             'maxTime','frameCount','frameTot','timeTot','fpsEst','radarType','pgen')
+%     end
 end
     
 %% Post process the raw data 
 % Baseband Conversion
 frameTot_bb = zeros(size(frameTot)); 
 for i = 1:frameCount
-    frameTot_bb(:,i) = NoveldaDDC(frameTot(:,i), 'X2', pgen, fs_hz); 
+    frameTot_bb(:,i) = NoveldaDDC(frameTot(:,i), radarSpecifier, pgen, fs_hz); 
 end
 
 % FFT of signal for each bin
 framesFFT = db(abs(fft(frameTot_bb,frameCount,2)));
 
 % Analyze FPS
-analyzeFPS(timeTot, fpsEst); 
+%analyzeFPS(timeTot, fpsEst); 
 
 %% Plotting 
 % Figure 1: FFT for each bin
@@ -254,6 +276,7 @@ ylabel('Range bin');
 xlabel('Frequency'); 
 
 % Figure 4: FFT plot for bins ranging from firstBin to lastBin 
+% TODO: Add ancho vs cayenne 
 firstBin = 220;
 lastBin = 240; 
 figure(4); plot(framesFFT(firstBin:lastBin,:)') 
