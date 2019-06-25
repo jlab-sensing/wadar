c241343566ebd67bf44aa10b6dcd07361af7124f
Colleen Josephson <cajoseph@stanford.edu>
2019-06-25 14:53:46 -0700

    clarified calibration equations

--- matlab/soil_moisture.m
+++ matlab/soil_moisture.m
@@ -11,7 +11,7 @@ radar_perm = ((c*t)/d)^2
 teros_perm = ((2.887e-9)*RAW^3-(2.080e-5)*RAW^2+(5.276e-2)*RAW-43.39)^2
 
 %TODO: adapt teros eqns to be used with EC instead of RAW
-%teros_calibrated = (1.059e-9)*q^6 - (8.088e-6)*q^4 + (2.066e-2)*q-1.727e1
-teros_potting_eqn = (1.059e-9)*RAW^3 - (8.088e-6)*RAW^2 + (2.066e-2)*RAW-1.727e1
+teros_calibrated = (1.059e-9)*RAW^3 - (8.088e-6)*RAW^2 + (2.066e-2)*RAW - 1.727e1 %calibrated by us
+teros_potting =   (6.771e-10)*RAW^3 - (5.105e-6)*RAW^2 + (1.302e-2)*RAW - 10.848 %from p. 8 of teros 12 manual
 radar_topp = (4.3e-6)*radar_perm^3 - (5.5e-4)*radar_perm^2 + (2.92e-2)*radar_perm - 5.3e-2
 teros_topp = (4.3e-6)*teros_perm^3 - (5.5e-4)*teros_perm^2 + (2.92e-2)*teros_perm - 5.3e-2
