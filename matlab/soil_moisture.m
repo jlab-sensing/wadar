e54b6482c674c5776c30e1ca2d4eccf8c5621702
cjosephson <cjoseph@mit.edu>
2019-06-30 19:23:09 -0700

    fixed an image

--- matlab/soil_moisture.m
+++ matlab/soil_moisture.m
@@ -3,11 +3,12 @@ c=299792458;
 resolution =  0.004; %0.003790984152165;
 airBins = 215; % number of bins from the radar to the un-burried tag
 airSoilBins = (240+240+239)/3.0; % number of bins from the radar to the burried tag
-RAW = 2345;  %RAW teros12 measurement
+RAW = 3500;  %RAW teros12 measurement
 d=0.055; % meters sensor is buried underground
 t=((airSoilBins-airBins+d/resolution)*resolution)/c; %seconds
 
-radar_perm = ((c*t)/d)^2
+
+radar_perm = ((c*t)/d)^2 % apparent dielectric permittivity
 teros_perm = ((2.887e-9)*RAW^3-(2.080e-5)*RAW^2+(5.276e-2)*RAW-43.39)^2
 
 %TODO: adapt teros eqns to be used with Perm instead of RAW
