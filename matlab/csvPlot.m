0775ff59bc782d7fda7c4d2c5d9116189016d76d
Colleen Josephson <cajoseph@stanford.edu>
2019-09-20 15:48:02 -0700

    sim prograve

--- matlab/csvPlot.m
+++ matlab/csvPlot.m
@@ -1,5 +1,5 @@
 % freq[Hz]; re:Trc1_S11; im:Trc1_S11; re:Trc2_S41; im:Trc2_S41; 
-sXY = dlmread('lgVivaldi.csv',';',1,0);
+sXY = dlmread('chipotleAnt.csv',';',1,0);
 f = sXY(:,1);
 
 cpx = zeros(500,1);
@@ -16,6 +16,6 @@ for i = 1:500
 end
 magS22 = db(abs(cpx));
 
-figure(); plot(f,magS22); title('Lg Vivaldi S11 '); ylim([-100,10]); grid on
+figure(); plot(f,magS22); title('Chipotle Vivaldi S11 '); ylim([-100,10]); grid on
 
 
