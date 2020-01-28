070aa884f09d51eb99d2aeae1c67a0d9acee059e
Bradley Barnhart <bbarnhar@stanford.edu>
2020-01-28 10:10:24 -0800

    SNR helper function

---
+++ matlab/calculateSNR.m
@@ -0,0 +1,23 @@
+% function to calculate signal to noise ratio.
+% calculates ratio of peak bin amplitude at desired 
+% frequency vs an average of a few irrelevant frequencies 
+
+% Args
+% ft: ft (bins x frequencies) - magnitude or complex
+% freq: desired frequency 
+% bin: selected bin 
+
+function SNR = calculateSNR(ft, freq, bin)
+
+signalMag = abs(ft(bin, freq));
+
+noiseFreq = round(freq*0.945 : freq*0.955);
+
+noiseMag = mean(abs(ft(bin, noiseFreq))); 
+
+SNR = signalMag / noiseMag; 
+SNRdb = 10 * log10(SNR); 
+
+end
+
+
