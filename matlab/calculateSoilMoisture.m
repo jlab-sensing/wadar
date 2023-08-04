3f31037437db3460a684b9729f1c77b52d16ad66
ericdvet <erikvetha@gmail.com>
2023-08-04 10:31:37 -0700

    updated calculateSoilMoisture.m with UCSC farm calibrations

--- matlab/calculateSoilMoisture.m
+++ matlab/calculateSoilMoisture.m
@@ -14,15 +14,18 @@ function [moisture] = calculateSoilMoisture(airBins, airSoilBins, soilType, d)
     %radar_calibrated = (1.059e-9)*perm_to_RAW^3 - (8.088e-6)*perm_to_RAW^2 + (2.066e-2)*perm_to_RAW - 1.727e1 %created by us
     %teros_calibrated = (1.059e-9)*RAW^3 - (8.088e-6)*RAW^2 + (2.066e-2)*RAW - 1.727e1 %calibrated by us
     if soilType == 'farm'
+        radar_farmcalibrated = (5.12018081e-10)*perm_to_RAW^3 - (0.000003854251138)*perm_to_RAW^2 + (0.009950433112)*perm_to_RAW - 8.508168835941; % Using Soil Calibration for UCSC Soil
+        moisture = radar_farmcalibrated; 
+    else if soilType == 'stanfordFarm'
         radar_farmcalibrated = (9.079e-10)*perm_to_RAW^3 - (6.626e-6)*perm_to_RAW^2 + (1.643e-2)*perm_to_RAW - 1.354e1;%created by us
         moisture = radar_farmcalibrated; 
-    elseif soilType == 'silt'
+    elseif soilType == 'stanfordSilt'
         radar_siltcalibrated = (-3.475e-10)*perm_to_RAW^3 + (2.263e-6)*perm_to_RAW^2 - (4.515e-3)*perm_to_RAW + 2.85e0;%created by us
         moisture = radar_siltcalibrated;
-    elseif soilType == 'clay' 
+    elseif soilType == 'stanfordClay' 
         radar_claycalibrated = (5.916e-10)*perm_to_RAW^3 - (4.536e-6)*perm_to_RAW^2 + (1.183e-2)*perm_to_RAW - 1.017e1;%created by us
         moisture = radar_claycalibrated; 
     else
-        error('need a legitimate soil type (farm, silt, clay)'); 
+        error('need a legitimate soil type (farm, stanfordFarm, stanfordSilt, stanfordClay)'); 
     end
 end
