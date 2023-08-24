28e6b54e9eb3e6a1260ef444081f786be3fe7990
ericdvet <erikvetha@gmail.com>
2023-08-24 12:58:42 -0700

    soil moisture calculation updated

--- matlab/calculateSoilMoisture.m
+++ matlab/calculateSoilMoisture.m
@@ -3,6 +3,8 @@ function [moisture] = calculateSoilMoisture(airBins, airSoilBins, soilType, d)
     c=299792458;
     resolution = 0.003790984152165; %0.004; % 
     t=((airSoilBins-airBins+d/resolution)*resolution)/c; %seconds
+
+    % t = d/c
     
     %Values
     radar_perm = ((c*t)/d)^2;
@@ -15,7 +17,7 @@ function [moisture] = calculateSoilMoisture(airBins, airSoilBins, soilType, d)
     %teros_calibrated = (1.059e-9)*RAW^3 - (8.088e-6)*RAW^2 + (2.066e-2)*RAW - 1.727e1 %calibrated by us
     if soilType == 'farm'
         radar_farmcalibrated = (5.12018081e-10)*perm_to_RAW^3 - (0.000003854251138)*perm_to_RAW^2 + (0.009950433112)*perm_to_RAW - 8.508168835941; % Using Soil Calibration for UCSC Soil
-        moisture = radar_farmcalibrated; 
+        moisture = radar_farmcalibrated;
     else if soilType == 'stanfordFarm'
         radar_farmcalibrated = (9.079e-10)*perm_to_RAW^3 - (6.626e-6)*perm_to_RAW^2 + (1.643e-2)*perm_to_RAW - 1.354e1;%created by us
         moisture = radar_farmcalibrated; 
