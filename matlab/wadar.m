349216aa66492c1775dac1916d5caccf95bb4e6e
Eric Vetha <evetha@ucsc.edu>
2023-11-14 09:17:11 -0800

    improved frequency isolation

--- matlab/wadar.m
+++ matlab/wadar.m
@@ -15,24 +15,25 @@ function vwc = wadar(airFileName, captureName)
 
 close all
 
-d = 0.12; % in meters
+d = 0.13; % in meters
 localDataPath = "C:/jlab/wadar/matlab/data/";
 tagHz = 80;
 airCaptureFile = airFileName;
-
-[freqTag, freqTagHar] = calculateTagFrequencies(tagHz, 200, 20000); 
+frameRate = 200;
 
 %% Load Air Capture %%
 [airRawFrames pgen fs_hz chipSet timeDeltas] = salsaLoad(fullfile(localDataPath, airCaptureFile));
 airFrameCount = size(airRawFrames, 2);
 airFramesBB = zeros(size(airRawFrames));
 
+[freqTag, freqTagHar] = calculateTagFrequencies(tagHz, 200, airFrameCount); 
+
 for j = 1:airFrameCount
     airFramesBB(:,j) = NoveldaDDC(airRawFrames(:,j), chipSet, pgen, fs_hz);
 end
 
 % Find the bin corresponding to the largest peak 
-airFT = fft(airFramesBB, 20000 , 2); 
+airFT = fft(airFramesBB, airFrameCount , 2); 
 airTagFT = abs(airFT(:, freqTag)); 
 [vals, peaks] = findpeaks(airTagFT, 'MinPeakHeight', max(airTagFT) * 0.9);
 airPeakBin = peaks(1);
@@ -47,15 +48,68 @@ for i = 1:frameCount
 end
 
 % FFT of signal for each bin
-[freqTag, freqTagHar] = calculateTagFrequencies(tagHz, 200, 20000);
-ft = fft(framesBB(:,1:frameCount),frameCount,2);
+ft = fft(framesBB(:,1:frameCount),frameCount,2);    
+
+% Find Tag FT
+freqTag = tagHz / frameRate * frameCount;
+freqTagHar = (frameRate - tagHz) / frameRate * frameCount; 
+
 TagFT = abs(ft(:, freqTag));
-[vals, peaks] = findpeaks(TagFT, 'MinPeakHeight', max(TagFT) * 0.90);
+for i = (freqTag-2:1:freqTag+2)
+    temp = abs(ft(:, i));
+    if max(temp(1:airPeakBin, :)) > max(temp(airPeakBin:end, :)) % if greatest peak is before air peak, skip
+        continue
+    end
+    if max(temp(airPeakBin:end)) > max(TagFT(airPeakBin:end))
+        TagFT = temp;
+    end
+end
+
+TagHarFT = abs(ft(:, freqTagHar));
+for i = (freqTagHar-2:1:freqTagHar+2)
+    temp = abs(ft(:, i));
+    if max(temp(1:airPeakBin, :)) > max(temp(airPeakBin:end, :)) % if greatest peak is before air peak, skip
+        continue
+    end
+    if max(temp(airPeakBin:end)) > max(TagFT(airPeakBin:end))
+        TagHarFT = temp;
+    end
+end
+
+TagFT = TagFT; % + TagHarFT;
+
+[~, peaks] = findpeaks(TagFT(airPeakBin:end), 'MinPeakHeight', max(TagFT(airPeakBin:end)) * 0.90);
+peaks = peaks+airPeakBin;
+
+% Select peak bin greater than air peak
 peak = peaks(1);
+i = 0;
+while peak < airPeakBin
+    if i > length(peaks) - 1
+        break;
+    end
+    i = i + 1;
+    peak = peaks(i);
+end
+% peak = 250;
+
+corrArray = zeros(512,1);
+for i = (1:1:512)
+    corrArray(i) = corr(circshift(airTagFT, i), TagFT);
+end
+figure(2)
+plot(circshift(corrArray, airPeakBin))
+xline(peak)
+[~, peakIndex] = max(circshift(corrArray, airPeakBin))
 
 vwc = calculateSoilMoisture(airPeakBin, peak, "farm", d);
 
-plot(TagFT)
+figure(1)
+plot(normalize(TagFT, "range"))
+hold on;
+plot(normalize(airTagFT, "range"))
+xline(peakIndex)
+legend('Capture (80 Hz + 120 Hz)', 'Air (80 Hz)')
 xlabel('Range Bins')
 ylabel('Magnitude')
 title("80 Hz Isolated");
