aa7486186f2b18c198ab412efb0f3c8a2a8f7c7a
Eric Vetha <erikvetha@gmail.com>
2024-07-12 12:43:49 -0700

    wadar.m usability updated

--- matlab/wadar.m
+++ matlab/wadar.m
@@ -1,4 +1,4 @@
-function volumetricWaterContent = wadar(displayAllFrames, airFramesName, templateFramesName, localDataPath, trialName, captureCount, tagDepth)
+function volumetricWaterContent = wadar(displayAllFrames, airFramesName, localDataPath, trialName, tagHz, frameCount, captureCount, tagDepth)
 % volumetricWaterContent = wadar(airFramesName, templateFramesName, localDataPath, trialName, captureCount)
 %
 % Script calculates the volumetric water content (vwc) of the soil from 
@@ -22,21 +22,13 @@ close all
 
 % Capture parameters
 frameRate = 200;   
-frameCount = 2000;
 radarType = 'Chipotle';
-fullDataPath = sprintf("ericdvet@192.168.7.1:%s",localDataPath);
 
 % Processing parameters
 soilType = 'farm';
 
-%% Process Template Capture
-[procResult, ~, templateTagFT, ~, ~] = procRadarFrames(localDataPath, templateFramesName);
-if (procResult == false)
-    error("ERROR: Template Frames Invalid")
-end
-
 %% Process Air Capture
-[procResult, ~, airTagFT, airPeakBin, ~] = procRadarFrames(localDataPath, airFramesName);
+[procResult, ~, airTagFT, airPeakBin, ~] = procRadarFrames(localDataPath, airFramesName, tagHz);
 if (procResult == false)
     error("ERROR: Air Frames Invalid")
 end
@@ -46,11 +38,14 @@ end
 [year, month, date] = ymd(datetime("now"));
 
 % File name generation
+[~, hostname] = system('whoami');
+hostname(end) = '';
+fullDataPath = sprintf("%s@192.168.7.1:%s", hostname, localDataPath);
 if isnumeric(trialName)
     trialName = num2str(trialName);
 end
 
-captureName = strcat(num2str(year), '-', num2str(month), '-', num2str(date), '_WetUnder', num2str(tagDepth*1000), 'mm_T', num2str(trialName), '_C');
+captureName = strcat(num2str(year), '-', num2str(month), '-', num2str(date), '_', num2str(tagDepth*1000), 'mmDepth_', num2str(trialName), '_C');
 
 % Check for existing files with the same name
 existingFiles = dir(localDataPath);
@@ -117,9 +112,9 @@ for i = 1:1:captureCount
     else
         fprintf('Framelogger captured frame succesfully!\n\n')
     end
-
+    
     wetFramesName = strcat(captureName, num2str(i), '.frames');
-    [procSuccess, wetPeakBin, airPeakBin, SNRdB(i), wetTagFT] = procCaptureCorrelation(templateFramesName, airFramesName, wetFramesName, localDataPath);
+    [procSuccess, captureFT, wetTagFT, wetPeakBin, SNRdB(i)] = procRadarFrames(localDataPath, wetFramesName, tagHz);
     if (procSuccess == false)
         failedCaptures = [failedCaptures i];
         fprintf("Capture %d will not be processed.\n", i)
@@ -136,11 +131,10 @@ for i = 1:1:captureCount
         clf(1)
         plot(normalize(wetTagFT, "range"))
         hold on;
-        plot(normalize(templateTagFT, "range"))
         plot(normalize(airTagFT, "range"))
         xline(airPeakBin)
         xline(wetPeakBin)
-        legend('Capture (80 Hz)', 'Template (80 Hz)', 'Air (80 Hz)')
+        legend('Capture (80 Hz)', 'Air (80 Hz)')
         xlabel('Range Bins')
         ylabel('Magnitude')
         title("VWC = ", num2str(vwc(i)));
@@ -155,110 +149,23 @@ for i = failedCaptures
     peakBin(i) = [];
 end
 
-% for i = 1:1:captureCount
-%     try
-%         [rawFrames, pgen, fs_hz, chipSet, ~] = salsaLoad(fullfile(localDataPath, strcat(captureName, num2str(i), '.frames')));
-%     catch
-%         failedCaptures = [failedCaptures i];
-%         continue
-%     end
-% 
-%     % Baseband Conversion
-%     frameCount = size(rawFrames, 2);
-%     framesBB = zeros(size(rawFrames));
-%     for j = 1:frameCount
-%         framesBB(:,j) = NoveldaDDC(rawFrames(:,j), chipSet, pgen, fs_hz);
-%     end
-% 
-%     % Find Tag FT
-%     freqTag = tagHz / frameRate * frameCount;
-%     captureFT = fft(framesBB, frameCount , 2); 
-% 
-%     % Find the bin corresponding to the largest peak 
-%     TagFT = abs(captureFT(:, freqTag));
-%     for j = (freqTag-2:1:freqTag+2)
-%         temp = abs(captureFT(:, j));
-%         if max(temp(1:airPeakBin, :)) > max(temp(airPeakBin:end, :)) % if greatest peak is before air peak, skip
-%             continue
-%         end
-%         if max(temp(airPeakBin:end)) > max(TagFT(airPeakBin:end))
-%             TagFT = temp;
-%         end
-%     end
-%     TagFT = smoothdata(TagFT, 'movmean', 10);
-% 
-%     % TagHarFT = abs(captureFT(:, freqTagHar));
-%     % for i = (freqTagHar-2:1:freqTagHar+2)
-%     %     temp = abs(captureFT(:, i));
-%     %     if max(temp(1:airPeakBin, :)) > max(temp(airPeakBin:end, :)) % if greatest peak is before air peak, skip
-%     %         continue
-%     %     end
-%     %     if max(temp(airPeakBin:end)) > max(TagFT(airPeakBin:end))
-%     %         TagHarFT = temp;
-%     %     end
-%     % end
-% 
-%     % Find the bin corresponding to the largest peak 
-%     [~, peaks] = findpeaks(TagFT(airPeakBin:end), 'MinPeakHeight', max(TagFT(airPeakBin:end)) * 0.90);
-%     peaks = peaks+airPeakBin;
-% 
-%     % Select peak bin greater than air peak
-%     peak = peaks(1);
-%     j = 0;
-%     while peak < airPeakBin
-%         if j > length(peaks) - 1
-%             break;
-%         end
-%         j = i + 1;
-%         peak = peaks(j);
-%     end
-% 
-%     % Select peak bin correlated to template capture
-%     % corrArray = zeros(length(peaks),2);
-%     corrArray = zeros(1, 512);
-%     for j = (1:1:512)
-%         corrArray(j) = corr(normalize(circshift(templateTagFT, j)), normalize(TagFT), 'Type', 'Pearson');
-%         % corrArray(j) = TagFT(j);
-%     end
-%     [~, peakIndex] = max(circshift(corrArray, templatePeakBin));
-%     closestPeak = peaks(1);
-%     for j = 1:size(peaks, 1)
-%         if abs(peaks(j) - peakIndex) < closestPeak
-%             closestPeak = peaks(j);
-%         end
-%     end
-%     peakBin(i) = closestPeak;
-% 
-%     vwc(i) = calculateSoilMoisture(airPeakBin, closestPeak, "farm", tagDepth);
-% 
-%     SNR(i) = calculateSNR(captureFT, freqTag, peakBin(i));
-%     SNRdB(i) = 10 * log10(SNR(i));
-% end
-% 
-% % Remove failed captures
-% for i = failedCaptures
-%     SNRdB(i) = [];
-%     peakMagnitudes(i) = [];
-%     peakBin(i) = [];
-% end
-
 figure(1)
 clf(1)
 plot(normalize(wetTagFT, "range"))
 hold on;
-plot(normalize(templateTagFT, "range"))
 plot(normalize(airTagFT, "range"))
 xline(airPeakBin)
 xline(wetPeakBin)
-legend('Capture (80 Hz)', 'Template (80 Hz)', 'Air (80 Hz)')
+legend('Capture (80 Hz)', 'Air (80 Hz)')
 xlabel('Range Bins')
 ylabel('Magnitude')
 title("80 Hz Isolated");
 
-fprintf("Air peak located at %d\n", airPeakBin);
-fprintf("Capture peak located at %d\n\n", wetPeakBin);
-
 fprintf("\nTesting Results\n\n")
+
+trialNumbers = 1:captureCount;
+results = table(trialNumbers, peakMagnitudes, SNRdB, peakBin, vwc);
+disp(results)
 
 fprintf("VWC:\nMean = %f\nMedian = %f\n\n", mean(vwc), median(vwc));
 
@@ -273,5 +180,27 @@ fprintf("Maximum difference between peak bins: %d\n\n", max(diff(sort(peakBin)))
 
 volumetricWaterContent = median(vwc);
 
+figure(3)
+% hold on
+% for j = 2:1:frameCount
+%     plot(abs(captureFT(:, j)))
+% end
+captureFT(:, 1:2) = ones(512, 2); % first 2 frames of capture are extremely noisy
+% processedFrames = frameCount/100;
+% captureFT(:, 1:processedFrames) = ones(512, processedFrames); 
+% captureFT(:, frameCount-processedFrames+1:frameCount) = ones(512, processedFrames); 
+x = (1:512)';
+y = (1:frameCount) / frameCount * frameRate;
+[xMat, yMat] = meshgrid(x, y);
+zMat = abs(captureFT(:, 1:frameCount))';
+surf(xMat, yMat, zMat, 'EdgeColor', 'none');
+ax = gca; % Get current axes
+% ax.FontSize = 18; % Set the desired font size for the ticks
+xlim([0 512])
+
+xlabel('Range Bins')
+ylabel('Frequency')
+zlabel('Magnitude')
+title(strcat("Capture", num2str(i), " - FT bins"));
 
 end
