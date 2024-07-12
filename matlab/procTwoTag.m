07dd49b0e1331cca38989a9c8ff23b07d8ed69ea
Eric Vetha <erikvetha@gmail.com>
2024-07-12 12:04:41 -0700

    w/ last commit

--- matlab/procTwoTag.m
+++ matlab/procTwoTag.m
@@ -13,7 +13,7 @@ function [procResult, captureFT, diffBins, tag1FT, tag2FT, tag1Peak, tag2Peak, t
 % Outputs:
 %       
 
-%% Processing parameters
+% %% Processing parameters
 frameRate = 200;  
 
 %% Load Capture
@@ -32,9 +32,9 @@ for j = 1:frameCount
     framesBB(:,j) = NoveldaDDC(rawFrames(:,j), chipSet, pgen, fs_hz);
 end
 
-
 % Find Tag FT
-captureFT = fft(framesBB, frameCount , 2); 
+% captureFT = fft(framesBB, frameCount , 2); 
+captureFT = fft(framesBB(:,1:frameCount),frameCount,2);
 
 freq1Tag = tag1Hz / frameRate * frameCount;
 tag1FT = abs(captureFT(:, freq1Tag));
@@ -46,27 +46,18 @@ for i = (freq1Tag-2:1:freq1Tag+2)
 end
 % tag1FT = smoothdata(tag1FT, 'movmean', 10);
 
-% Find the bin corresponding to the largest peak 
-[~, peaks] = findpeaks(tag1FT, 'MinPeakHeight', max(tag1FT) * 0.9);
-if (size(peaks, 1) > 1)
-    if (peaks(2) - peaks(1) < 50)
-        peakBin = peaks(1) +  round((peaks(2) - peaks(1)) / 2) + round((tag1FT(peaks(2)) - ...
-            tag1FT(peaks(1))) / max(tag1FT) * (peaks(2) - peaks(1)));
-    else
-        peakBin = peaks(1);
+freq1TagHar = (frameRate - tag1Hz) / frameRate * frameCount;
+tag1FTHar = abs(captureFT(:, freq1TagHar));
+for i = (freq1TagHar-2:1:freq1TagHar+2)
+    temp = abs(captureFT(:, i));
+    if max(temp) > max(tag1FTHar)
+        tag1FTHar = temp;
     end
-elseif (size(peaks, 1) == 1)
-    peakBin = peaks(1);
-else
-    peakBin = -1;
 end
+% tag1FTHar = smoothdata(tag1FT, 'movmean', 10);
 
-peakBins(1) = peakBin;
+tag1FT = tag1FT;
 
-SNR = calculateSNR(captureFT, freq1Tag, peakBin);
-SNRdB(1) = 10 * log10(SNR);
-
-peakMagnitudes(1) = tag1FT(peakBin);
 
 freq2Tag = tag2Hz / frameRate * frameCount;
 tag2FT = abs(captureFT(:, freq2Tag));
@@ -78,80 +69,65 @@ for i = (freq2Tag-2:1:freq2Tag+2)
 end
 % tag2FT = smoothdata(tag2FT, 'movmean', 10);
 
-% Find the bin corresponding to the largest peak 
-[~, peaks] = findpeaks(tag2FT, 'MinPeakHeight', max(tag2FT) * 0.9);
-if (size(peaks, 1) > 1)
-    if (peaks(2) - peaks(1) < 50)
-        peakBin = peaks(1) +  round((peaks(2) - peaks(1)) / 2) + round((tag2FT(peaks(2)) - ...
-            tag2FT(peaks(1))) / max(tag2FT) * (peaks(2) - peaks(1)));
-    else
-        peakBin = peaks(1);
+freq2TagHar = (frameRate - tag2Hz) / frameRate * frameCount;
+tag2FTHar = abs(captureFT(:, freq2TagHar));
+for i = (freq2TagHar-2:1:freq2TagHar+2)
+    temp = abs(captureFT(:, i));
+    if max(temp) > max(tag2FTHar)
+        tag2FTHar = temp;
     end
-elseif (size(peaks, 1) == 1)
-    peakBin = peaks(1);
-else
-    peakBin = -1;
 end
+% tag2FTHar = smoothdata(tag2FT, 'movmean', 10);
 
-peakBins(2) = peakBin;
+tag2FT = tag2FT;
 
-SNR = calculateSNR(captureFT, freq2Tag, peakBin);
-SNRdB(2) = 10 * log10(SNR);
+peakBins(1) = procCaptureCWT(tag1FT, true);
+peakBins(2) = procCaptureCWT(tag2FT, false);
 
-peakMagnitudes(2) = tag2FT(peakBin);
-
-diffBins = abs(peakBins(2) - peakBins(1));
+diffBins = abs(peakBins(1) - peakBins(2));
 tag1Peak = peakBins(1);
 tag2Peak = peakBins(2);
-tag1SNR = SNRdB(1);
-tag2SNR = SNRdB(2); % TODO, fix SNR for dual tags
+
+tag1SNR = 0;
+tag2SNR = 0;
 
 %% Display Results
 if (resultFlag == true) 
     
-    % fprintf("\n%s Testing Results\n\n", captureName)
-    % 
-    % 
-    % fprintf("Peak Magnitude Results:\n")
-    % fprintf("%d Hz Tag: %f\n", tag1Hz, peakMagnitudes(1))
-    % fprintf("%d Hz Tag: %f\n\n", tag2Hz, peakMagnitudes(2))
-    % 
-    % fprintf("Peak Results:\n")
-    % fprintf("%d Hz Tag: %d\n", tag1Hz, peakBins(1))
-    % fprintf("%d Hz Tag: %d\n\n", tag2Hz, peakBins(2))
-    % 
-    % fprintf("Peak Bin Difference:\n")
-    % fprintf("|%d - %d| = %d", peakBins(1), peakBins(2), abs(peakBins(1) - peakBins(2)))
-    
     close all
+
     figure(1)
-    hold on;
-    plot(tag1FT, 'displayname', sprintf('%i Hz FFT bin of tag 1',tag1Hz)); hold on;
-    plot(tag2FT, 'DisplayName', sprintf('%i Hz FFT bin of tag 2',tag2Hz)); hold on; 
-    plot(peakBins(1), tag1FT(peakBins(1)), 'o', 'DisplayName', sprintf('detected peak = %i', peakBins(1)));
-    plot(peakBins(2), tag2FT(peakBins(2)), 'o', 'DisplayName', sprintf('detected peak = %i', peakBins(2)));
-    legend(sprintf('%i Hz FFT bin of tag 1',tag1Hz), sprintf('%i Hz FFT bin of tag 2',tag2Hz))
-    xlabel('Range Bins')
-    ylabel('Magnitude')
-    title(captureName);
+    plot(tag2FT)
+    hold on
+    plot(tag2FT)
+    scatter(peakBins(1), tag1FT(peakBins(1)), "x")
+    scatter(peakBins(2), tag2FT(peakBins(2)), "x")
+    legend(sprintf("%d Hz @ %d", tag1Hz, peakBins(1)), sprintf("%d Hz @ %d", tag2Hz, peakBins(2)))
+    title(sprintf("%s FT", captureName))
+    xlabel("Range Bins")
+    ylabel("Normalized Magnitude")
+    % title('Tag FT with optimal antenna alignment')
     
-    % figure(2)
-    % % hold on
-    % % for j = 2:1:frameCount
-    % %     plot(abs(captureFT(:, j)))
-    % % end
-    % captureFT(:, 1:2) = ones(512, 2); % first 2 frames of capture is extremely noisy
-    % x = (1:1:512)';
-    % y = (1:1:frameCount) / frameCount * frameRate;
-    % xMat = repmat(x, 1, length(y));
-    % yMat = repmat(y, length(x), 1);
-    % zMat = abs(captureFT(:, 1:frameCount));
-    % plot3(xMat, yMat, zMat)
-    % 
-    % xlabel('Range Bins')
-    % ylabel('Frequency')
-    % zlabel('Magnitude')
-    % title(strcat("Capture", " - FT of all peak bins"));
+    figure(2)
+
+    captureFT(:, 1:2) = ones(512, 2); % first 2 frames of capture are extremely noisy
+    % processedFrames = frameCount/100;
+    % captureFT(:, 1:processedFrames) = ones(512, processedFrames); 
+    % captureFT(:, frameCount-processedFrames+1:frameCount) = ones(512, processedFrames); 
+    x = (1:512)';
+    y = (1:frameCount) / frameCount * frameRate;
+    [xMat, yMat] = meshgrid(x, y);
+    zMat = abs(captureFT(:, 1:frameCount))';
+    surf(xMat, yMat, zMat, 'EdgeColor', 'none');
+    ax = gca; % Get current axes
+    ax.FontSize = 18; % Set the desired font size for the ticks
+    xlim([0 512])
+
+
+    xlabel('Range Bins', 'FontSize', 22)
+    ylabel('Frequency', 'FontSize', 22)
+    zlabel('Magnitude', 'FontSize', 22)
+    title(strcat("Capture", " - FT of all peak bins"), 'FontSize', 30);
 end
 
 end
