ecfa72eac532a43be497054b9192623749c7f1a7
Eric Vetha <erikvetha@gmail.com>
2025-01-03 17:26:24 -0800

    robot localization probably works but its raining

--- b1_ws/src/nav_farm_walker/nav_farm_walker/sensor_publisher.py
+++ b1_ws/src/nav_farm_walker/nav_farm_walker/sensor_publisher.py
@@ -2,45 +2,43 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Imu, NavSatFix
 from nav_msgs.msg import Odometry
-from inertial_sense_ros2.msg import DIDINS2, GPS
+from inertial_sense_ros2.msg import DIDINS2, DIDINS4, GPS
 
 class SensorPublisher(Node):
 
     def __init__(self):
         super().__init__('sensor_publisher')
-        self.imu_pub = self.create_publisher(Imu, '/imu/data', 10)
+
+        self.imu_sub = self.create_subscription(Imu, '/imu', self.imu_callback, 10) 
+        self.imu_pub = self.create_publisher(Imu, '/imu/data', 10)  
+
+        self.gps_sub = self.create_subscription(GPS, '/gps1/pos_vel', self.gps_callback, 10)  
         self.gps_pub = self.create_publisher(NavSatFix, '/gps/fix', 10)
-        self.odom_pub = self.create_publisher(Odometry, '/odometry/gps', 10)
-        self.odom_pub_additional = self.create_publisher(Odometry, '/odom', 10)  # Additional publisher for /odom
-        self.ins_sub = self.create_subscription(DIDINS2, '/ins_quat_uvw_lla', self.ins_callback, 10)  # Subscription to DIDINS2
-        self.gps_sub = self.create_subscription(GPS, '/gps1/pos_vel', self.gps_callback, 10)  # Subscription to GPS data
+        
+        self.odom_sub = self.create_subscription(Odometry, '/odom_ins_enu', self.odom_callback, 10)
+        self.odom_pub = self.create_publisher(Odometry, '/odom', 10)  
 
-    def ins_callback(self, data):
+    def imu_callback(self, data):
         self.publish_imu_data(data)
-        self.publish_odometry_data(data)
-
-    def gps_callback(self, data):
-        self.publish_gps_data(data)
 
     def publish_imu_data(self, data):
         imu_msg = Imu()
         imu_msg.header.stamp = self.get_clock().now().to_msg()
         imu_msg.header.frame_id = "imu_link"
-        imu_msg.orientation_covariance = [0.0] * 9
-        imu_msg.angular_velocity_covariance = [0.0] * 9
-        imu_msg.linear_acceleration_covariance = [0.0] * 9
-        imu_msg.orientation.x = float(data.qn2b[0])
-        imu_msg.orientation.y = float(data.qn2b[1])
-        imu_msg.orientation.z = float(data.qn2b[2])
-        imu_msg.orientation.w = float(data.qn2b[3])
-        imu_msg.angular_velocity.x = float(data.uvw[0])
-        imu_msg.angular_velocity.y = float(data.uvw[1])
-        imu_msg.angular_velocity.z = float(data.uvw[2])
-        imu_msg.linear_acceleration.x = float(data.uvw[0])
-        imu_msg.linear_acceleration.y = float(data.uvw[1])
-        imu_msg.linear_acceleration.z = float(data.uvw[2])
-
+        imu_msg.orientation.x = data.orientation.x
+        imu_msg.orientation.y = data.orientation.y
+        imu_msg.orientation.z = data.orientation.z
+        imu_msg.orientation.w = data.orientation.w
+        imu_msg.angular_velocity.x = data.angular_velocity.x
+        imu_msg.angular_velocity.y = data.angular_velocity.y
+        imu_msg.angular_velocity.z = data.angular_velocity.z
+        imu_msg.linear_acceleration.x = data.linear_acceleration.x
+        imu_msg.linear_acceleration.y = data.linear_acceleration.y
+        imu_msg.linear_acceleration.z = data.linear_acceleration.z
         self.imu_pub.publish(imu_msg)
+    
+    def gps_callback(self, data):
+        self.publish_gps_data(data)
 
     def publish_gps_data(self, data):
         gps_msg = NavSatFix()
@@ -50,24 +48,15 @@ class SensorPublisher(Node):
         gps_msg.longitude = data.longitude
         gps_msg.altitude = data.altitude
         gps_msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_UNKNOWN
-
         self.gps_pub.publish(gps_msg)
 
-    def publish_odometry_data(self, data):
-        odom_msg = Odometry()
-        odom_msg.header.stamp = self.get_clock().now().to_msg()
-        odom_msg.header.frame_id = "odom"
-        odom_msg.child_frame_id = "base_link"
-        odom_msg.pose.pose.orientation.x = float(data.qn2b[0])
-        odom_msg.pose.pose.orientation.y = float(data.qn2b[1])
-        odom_msg.pose.pose.orientation.z = float(data.qn2b[2])
-        odom_msg.pose.pose.orientation.w = float(data.qn2b[3])
-        odom_msg.pose.pose.position.x = float(data.lla[0])
-        odom_msg.pose.pose.position.y = float(data.lla[1])
-        odom_msg.pose.pose.position.z = float(data.lla[2])
+    def odom_callback(self, data):
+        self.publish_odom_data(data)
 
+    def publish_odom_data(self, data):
+        odom_msg = Odometry()
+        odom_msg = data
         self.odom_pub.publish(odom_msg)
-        self.odom_pub_additional.publish(odom_msg)  # Publish to /odom
 
 def main(args=None):
     rclpy.init(args=args)
