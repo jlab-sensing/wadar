6250b8d53b8a1492a13d76f38fd04b1197332566
ericdvet <ericdvet@gmail.com>
2025-07-23 12:14:58 -0700

    CNN feature extraction

--- 01_dsp/python/_04_athena/pretrained_cnn.py
+++ 01_dsp/python/_04_athena/pretrained_cnn.py
@@ -12,6 +12,207 @@ from PIL import Image
 import time
 from sklearn.model_selection import KFold
 
+class PretrainedCNNFeatureExtractor:
+    """
+    Pretrained CNN model for image classification. Much of the code is adapter from 
+    https://www.tensorflow.org/tutorials/images/transfer_learning.
+
+    Args:
+        X (np.ndarray):                Array of images of shape (samples, height, width). 
+                                       For raw radargrams, it should be of shape (samples, fast time, slow time).
+        y (np.ndarray):                Array of labels of shape (samples,).
+        output_dir (str):              Directory to save images and labels.
+        img_size (tuple):              Size to which images will be resized (default: (160, 160)).
+        batch_size (int):              Batch size for training (default: 32).
+    """
+
+    def __init__(self, X, y, output_dir, dimensions=128, img_size=(160, 160), batch_size=32):
+        """
+        Initialize the PretrainedCNN with images and labels.
+
+        Args:
+            X (np.ndarray):                Array of images of shape (samples, height, width).
+            y (np.ndarray):                Array of labels of shape (samples,).
+            output_dir (str):              Directory to save images and labels.
+            img_size (tuple):              Size to which images will be resized (default: (160, 160)).
+            batch_size (int):              Batch size for training (default: 32
+
+        Returns:
+            None
+        """
+
+        self.X = X
+        self.y = y
+        self.output_dir = output_dir
+        self.img_size = img_size
+        self.dimensions = dimensions
+        self.batch_size = batch_size
+        os.makedirs(self.output_dir, exist_ok=True)
+        self.labels = []
+        self.df = None
+        self.model = None
+    
+    def full_monty(self, epochs=10):
+        """
+        Run the full training and evaluation process.
+
+        Args:
+            epochs (int):                 Number of epochs for training (default: 10).
+
+        Returns:
+            model (tf.keras.Model):       The trained model.
+            metrics (dict):               Dictionary containing evaluation metrics.
+        """
+
+        self.prepare_data()
+        model, features = self.train_full(epochs=epochs)
+
+        return model, features
+
+    def prepare_data(self):
+        """
+        Prepare the dataset by saving images and labels to the output directory.
+
+        Returns:
+            None
+        """
+
+        # Save images and labels
+        for idx, sample in enumerate(self.X):
+            sample_min = sample.min()           # normalize to 0-255
+            sample_max = sample.max()
+            normalized_sample = 255 * (sample - sample_min) / (sample_max - sample_min + 1e-8)
+            img = Image.fromarray(normalized_sample.astype(np.uint8))
+            label = self.y[idx]
+            file_name =  f"sample_{idx:03d}.png"
+            img.save(os.path.join(self.output_dir, file_name))
+            self.labels.append({'filename': file_name, 'label': label})
+
+        print(f"Saved {len(self.X)} images to {self.output_dir}")
+        self.df = pd.DataFrame(self.labels)
+        self.df.to_csv(os.path.join(self.output_dir, "labels.csv"), index=False)
+
+
+    def load_dataset(self):
+        """
+        Load the dataset from the output directory.
+
+        Returns:
+            images (np.ndarray):           Array of images of shape (samples, height, width, 3).
+            labels (np.ndarray):           Array of labels of shape (samples,).
+        """
+
+        df = pd.read_csv(os.path.join(self.output_dir, "labels.csv"))
+
+        def load_image(filename):
+            img_path = os.path.join(self.output_dir, filename)
+            img = Image.open(img_path).convert('RGB')
+            img = img.resize(self.img_size)
+            return np.array(img)
+
+        images = []
+        labels = []
+
+        for idx, row in df.iterrows():
+            img_array = load_image(row['filename'])
+            images.append(img_array)
+            labels.append(float(row['label']))  
+
+        images = np.stack(images)
+        labels = np.array(labels)
+
+        return images, labels
+
+    def build_model(self, train_dataset=None):
+        """
+        Build the CNN model using a pre-trained MobileNetV2 as the base model.
+
+        Args:
+            train_dataset (tf.data.Dataset): Dataset for training (optional, used for input shape).
+
+        Returns:
+            None
+        """
+
+        # Rescale pixel values 
+        rescale = tf.keras.layers.Rescaling(1./127.5, offset=-1) # Rescale from [0, 255] to [-1, 1]
+
+        # ====================================================
+        # Create the base model from the pre-trained convnets
+        # ====================================================
+
+        # Create the base model from the pre-trained model MobileNet V2
+        IMG_SHAPE = self.img_size + (3,)
+        base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                                       include_top=False,
+                                                       weights='imagenet')
+        
+        image_batch, label_batch = next(iter(train_dataset))
+        feature_batch = base_model(image_batch)
+        print(feature_batch.shape)
+
+        # =====================================================
+        # Feature extraction
+        # =====================================================
+
+        base_model.trainable = False
+
+        # Let's take a look at the base model architecture
+        base_model.summary()
+
+        # No head needed for feature extraction
+
+        # global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+        # feature_batch_average = global_average_layer(feature_batch)
+        # print(feature_batch_average.shape)
+
+        # prediction_layer = tf.keras.layers.Dense(1)
+        # prediction_batch = prediction_layer(feature_batch_average)
+        # print(prediction_batch.shape)
+
+        inputs = tf.keras.Input(shape=IMG_SHAPE)
+        x = rescale(inputs)
+        x = base_model(x, training=False)
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        x = tf.keras.layers.Dense(self.dimensions)(x) # projecting to (samples x self.dimensions)
+        x = tf.keras.layers.Dropout(0.2)(x)
+        # outputs = prediction_layer(x)
+        self.model = tf.keras.Model(inputs, x)
+
+        self.model.summary()
+        print("Number of trainable variables:", len(self.model.trainable_variables))
+
+    def train_full(self, epochs=10):
+        """
+        Train the model with the entire dataset. Should only be used after cross-validation, as any validation
+        done with this method will not be representative.
+
+        Args:
+            epochs (int):                 Number of epochs for training (default: 10).
+
+        Returns:
+            None
+        """
+
+        images, labels = self.load_dataset()
+
+        train_ds = Dataset.from_tensor_slices((images, labels)).batch(self.batch_size)
+
+        AUTOTUNE = tf.data.AUTOTUNE
+        train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+        train_dataset = train_ds
+
+        self.build_model(train_dataset) # in case it was not called before
+
+        base_learning_rate = 0.0001
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
+                           loss=tf.keras.losses.MeanAbsoluteError(),
+                           metrics=[tf.keras.metrics.MeanAbsoluteError(name='mae')])
+        
+        features = self.model.predict(images)
+
+        return self.model, features
+
 class PretrainedCNNRegressor:
     """
     Pretrained CNN model for image classification. Much of the code is adapter from 
--- 01_dsp/python/tests/04_regression_tests/pretrained_cnn.py
+++ 01_dsp/python/tests/04_regression_tests/pretrained_cnn.py
@@ -7,7 +7,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.par
 sys.path.insert(0, parent_dir)
 
 from _01_gaia.loader import FrameLoader
-from _04_athena.pretrained_cnn import PretrainedCNNClassifier, PretrainedCNNRegressor
+from _04_athena.pretrained_cnn import PretrainedCNNClassifier, PretrainedCNNRegressor, PretrainedCNNFeatureExtractor
 from _06_hermes.logger import update_results
 
 if __name__ == "__main__":
@@ -20,10 +20,22 @@ if __name__ == "__main__":
 
     output_dir = "../../data/combined-soil-compaction-dataset/images"
 
-    trainer = PretrainedCNNRegressor(X, y, output_dir)
-    model, metrics = trainer.full_monty(epochs=50)
-    print("Cross-validation metrics:", metrics)
-    update_results("Pretrained CNN", metrics['mae'], metrics['accuracy'], metrics['inference_time'], dataset_dir)
+    feature_extractor = PretrainedCNNFeatureExtractor(X, y, output_dir, dimensions=2)
+    _, features = feature_extractor.full_monty()
+    print("Extracted features shape:", features.shape)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(features[:, 0], features[:, 1], c=y, cmap='viridis', s=10)
+    plt.colorbar(label='Labels')
+    plt.title('Feature Extraction using Pretrained CNN')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.show()
+
+    # trainer = PretrainedCNNRegressor(X, y, output_dir)
+    # model, metrics = trainer.full_monty(epochs=50)
+    # print("Cross-validation metrics:", metrics)
+    # update_results("Pretrained CNN", metrics['mae'], metrics['accuracy'], metrics['inference_time'], dataset_dir)
 
 
     # trainer = PretrainedCNNClassifier(X, y, output_dir)
