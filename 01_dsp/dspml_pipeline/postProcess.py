442d811ecec92672da156b31704a86028a424bb5
nubby <jlee211@ucsc.edu>
2025-11-13 00:54:43 -0800

    Functional post-processing code.

--- 01_dsp/dspml_pipeline/postProcess.py
+++ 01_dsp/dspml_pipeline/postProcess.py
@@ -16,6 +16,8 @@ import argparse
 import csv
 import os
 
+import numpy as np
+
 from typing import Any, List
 
 
@@ -29,6 +31,14 @@ def _read_csv(path: str) -> List[dict]:
             rows.append(row)
     return rows
 
+def _write_csv(data: List[str], path: str):
+    """
+    """
+    with open(path, "w") as csvp:
+        writer = csv.writer(csvp, delimiter=",")
+        for row in data:
+            writer.writerow(row)
+
 def process_input_file(path: str):
     """
     """
@@ -38,8 +48,10 @@ def process_input_file(path: str):
     else:
         print(f"Bummer!: {path}")
 
-def combine_results(paths: List[str]):
-    # Group results.
+def ingest_results_paths(paths: List[str]) -> dict:
+    """
+    Convert CSV file paths into formatted data.
+    """
     results = {}
     for path in paths:
         data = _read_csv(path)
@@ -52,20 +64,72 @@ def combine_results(paths: List[str]):
 
             if label not in results.keys():
                 results[label] = {
-                    "R2": [],
-                    "RMSE": [],
-                    "Accuracy": []
+                    "R2": { "Raw": [], "Mean": 0.0, "STD": 0.0 },
+                    "RMSE": { "Raw": [], "Mean": 0.0, "STD": 0.0 },
+                    "Accuracy": { "Raw": [], "Mean": 0.0, "STD": 0.0 },
+                    "N": 0
                 }
 
             try:
-                results[label]["R2"].append(row["R2"])
-                results[label]["RMSE"].append(row["RMSE"])
-                results[label]["Accuracy"].append(row["Accuracy"])
+                results[label]["R2"]["Raw"].append(float(row["R2"]))
+                results[label]["RMSE"]["Raw"].append(float(row["RMSE"]))
+                results[label]["Accuracy"]["Raw"].append(float(row["Accuracy"]))
+                results[label]["N"] += 1
             except KeyError:
                 print(f"WARNING! - Skipping {path} since these results are bad!: {e}")
                 continue
+    return results
 
-    print(results)
+def process_results(results: dict):
+    """
+    """
+    for label in results.keys():
+        for metric in ["R2", "RMSE", "Accuracy"]:
+            try:
+                results[label][metric]["Mean"] = np.mean(
+                    np.array(results[label][metric]["Raw"])
+                )
+                results[label][metric]["STD"] = np.std(
+                    np.array(results[label][metric]["Raw"])
+                )
+            except Exception as e:
+                print(f"ERROR: We got {e} from {results[label][metric]['Raw']}")
+
+def combine_results(paths: List[str]):
+    """
+    """
+    results = ingest_results_paths(paths)
+    process_results(results)
+    return results
+
+def save_results(results: dict, base_path: str):
+    """
+    """
+    data = []
+    # First add a header. 
+    data.append([
+        "Feature-Model",
+        "R2 (mean)",
+        "R2 (std)",
+        "RMSE (mean)",
+        "RMSE (std)",
+        "N"
+    ])
+
+    # Next format results into a list of CSV rows.
+    for key in results.keys():
+        data.append([
+            key,
+            results[key]["R2"]["Mean"],
+            results[key]["R2"]["STD"],
+            results[key]["RMSE"]["Mean"],
+            results[key]["RMSE"]["STD"],
+            results[key]["N"]
+        ])
+
+    # Combined data path.
+    combined_path = os.path.join(base_path, "results_val-combined.csv")
+    _write_csv(data, combined_path)
 
 def process_input_dir(path: str):
     """
@@ -84,7 +148,18 @@ def process_input_dir(path: str):
     ]
 
     # Collect combined results.
-    results = combine_results([p for p in csv_paths if "results" in p])
+    results = combine_results([p for p in csv_paths if "val_results" in p])
+
+    # Present or save results.
+    save_results(results=results, base_path=path)
+    """
+    [print(f"{label} - {metric} = {results[label][metric]['Mean']}") for metric in [
+        #"R2", "RMSE", "Accuracy"
+            "R2", "RMSE"
+        ] for label in results.keys()
+    ]
+    """
+
 
 def post_process(path_input_file: str, path_input_dir: str):
     """
